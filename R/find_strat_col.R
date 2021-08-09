715a7b473dedde460fbcff95ea5cff40d6c0079b
Andy Choens <andy.choens@acuitashealth.com>
2021-08-09 08:53:44 -0400

    Fixes failures from missing config$has_stratification

--- R/find_strat_col.R
+++ R/find_strat_col.R
@@ -2,12 +2,8 @@
 #'
 #' From a data frame/tibble, find the column labeled "Stratification".
 #' This is the column we will use to stratify the reports by.
-#' If config$has_stratification is set to:
-#' -  'strict', this function will return an error on any query that
-#'    doesn't have a stratification column.
-#' - 'relaxed', it will only return a warning if it doesn't find a
-#'   Stratification column and return NA.
-#' - 'no', it will return NA.
+#' If there isn't a stratification column, find_strat_col returns NA.
+#'
 #' @param data The data frame to search through.
 #' @param col_name The column name value of the stratification column. By
 #' default, this is 'Stratification'. It can, however, be anything else you
@@ -24,14 +20,8 @@ find_strat_col <- function(data = NULL, col_name = "Stratification") {
     }
 
     if (!col_name %in% names(data)) {
-        if (config$has_stratification == "strict") {
-            stop("There is no 'Stratification' column in 'data'.")
-        } else if (config$has_stratification == "relaxed") {
-            warning("There is no 'Stratification' column in 'data'.")
-            strat_col <- NA
-        } else {
-            strat_col <- NA
-        }
+        warning("There is no 'Stratification' column in 'data'.")
+        strat_col <- NA
     } else {
         strat_col <- which(stringr::str_detect(col_name, names(data)))
         if (length(strat_col) > 1) {
